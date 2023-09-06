5a614a56e353dd6b86bed78d6c55a5042771964b
tmbrwd <tmbrwd@gmail.com>
2023-09-06 09:36:41 +0800

    refinement

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,9 +22,12 @@ function Navbar() {
     <nav className="sticky top-0 z-10 bg-white p-5 backdrop-filter backdrop-blur-lg bg-opacity-80">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <span className="text-2xl text-[#051C2C] font-semibold navbar-text">
-            <Kmeaka />
-          </span>
+          <Link href='/'>
+
+            <span className="text-2xl text-[#051C2C] font-semibold navbar-text">
+              <Kmeaka />
+            </span>
+          </Link>
           <div className="flex space-x-4 text-[#051C2C]">
             <Link href="/">
               <p className="navbar-text font-bold text-xl hidden sm:inline hover:underline decoration-primary translate-x-3 transition-all  duration-100 delay-75">{translate('home')}</p>
@@ -49,10 +52,10 @@ function Navbar() {
         <div className={toggle ? 'block' : 'hidden'}>
           <div className="px-2 pt-2 pb-3 space-y-1">
             <Link href="/">
-            <p className="block text-base font-medium text-[#051C2C] hover:text-gray-700">{translate('home')}</p>
+              <p className="block text-base font-medium text-[#051C2C] hover:text-gray-700">{translate('home')}</p>
             </Link>
             <Link href="/">
-            <p className="block text-base font-medium text-[#051C2C] hover:text-gray-700">{translate('companies')}</p>
+              <p className="block text-base font-medium text-[#051C2C] hover:text-gray-700">{translate('companies')}</p>
             </Link>
           </div>
         </div>
--- src/pages/kontas.tsx
+++ src/pages/kontas.tsx
@@ -4,63 +4,73 @@ import { BsTelephone } from 'react-icons/bs';
 import { AiOutlineMail } from 'react-icons/ai'
 import { eMail, phoneNumber } from '@/constants';
 import { HiOutlineUserGroup } from 'react-icons/hi'
+import { BsFacebook, BsFillTelephoneFill } from 'react-icons/bs'
+import { MdLocationPin } from 'react-icons/md'
 import Kmeaka from '@/components/Kmeaka';
+import Link from 'next/link';
 
 function Kontas() {
     const { translate } = useContext(LanguageContext);
     return (
         <div className='bg-white'>
             <div className="w-full h-[580px]">
-                <div style={{ backgroundImage: `url('https://source.unsplash.com/uDSMTV06s4U')` }} className='w-full h-full bg-center bg-cover '>
+                <div style={{ backgroundImage: `url('/kontas.jpg')` }} className='w-full h-full bg-center bg-cover '>
                     <div className="backdrop-filter w-full h-full backdrop-brightness-50  text-[64px] flex justify-center p-10 text-white">
                         <p className='my-auto'>{translate('watchShop')}</p>
                     </div>
                 </div>
             </div>
             <div className="bg-[#F9F9FB] p-5 flex justify-between w-full">
-                <div className="mx-auto">
+                <div className="mx-auto  transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105 duration-150">
                     <div className="flex justify-center">
                         <div className="bg-primary-soft mx-5 rounded-full p-5 h-16 w-16 flex items-center justify-center">
-                            <HiOutlineUserGroup className='text-2xl' />
+                            <MdLocationPin className='text-2xl' />
                         </div>
                     </div>
-                    <div className="flex justify-center font-bold text-xl m-auto my-2">
-                        <p className="text-center">{translate('locations')}</p>
+                    <Link href='https://goo.gl/maps/zgTsNUHDWeV1K83z5'>
+
+                        <div className="flex justify-center font-bold text-xl m-auto my-2">
+                            <p className="text-center">{translate('locations')}</p>
+                        </div>
+
+                        <div className="flex justify-center font-light text-lg m-auto my-2">
+                            <p className="text-center">2nd khoroo - 21,<br /> Ulaanbaatar, Mongolia</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className="mx-auto  transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105 duration-150">
+                    <div className="flex justify-center">
+                        <div className="bg-primary-soft mx-5 rounded-full p-5 h-16 w-16 flex items-center justify-center">
+                            <BsFacebook className='text-2xl' />
+                        </div>
+                    </div>
+                    <Link href="https://www.facebook.com/kontasshop">
+                        <div className="flex justify-center font-bold text-xl m-auto my-2 hover:shadow-orange-300">
+                            <p className="text-center">{translate('social address')}</p>
+                        </div>
+                        <div className="flex justify-center font-light text-lg m-auto my-2">
+                            <p className="text-center">Kontas.mn</p>
+                        </div>
+                    </Link>
+                </div>
+                <div className="mx-auto  transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-105 duration-150">
+                    <div className="flex justify-center">
+                        <div className="bg-primary-soft mx-5 rounded-full p-5 h-16 w-16 flex items-center justify-center">
+                            <BsFillTelephoneFill className='text-2xl' />
+                        </div>
                     </div>
 
+                    <div className="flex justify-center font-bold text-xl m-auto my-2 hover:shadow-orange-300">
+                        <p className="text-center">{translate('contact-number')}</p>
+                    </div>
                     <div className="flex justify-center font-light text-lg m-auto my-2">
-                        <p className="text-center">MN Tower Mongolia, J.Sambuu St, Улаанбаатар</p>
+                        <p className="text-center">
+                            <a href="tel: 88116909"></a>+976 8811 6909</p>
                     </div>
 
                 </div>
-                <div className="mx-auto">
-                    <div className="flex justify-center">
-                        <div className="bg-primary-soft mx-5 rounded-full p-5 h-16 w-16 flex items-center justify-center">
-                            <HiOutlineUserGroup className='text-2xl' />
-                        </div>
-                    </div>
-                    <div className="flex justify-center font-bold text-xl m-auto my-2">
-                        <p className="text-center">{translate('locations')}</p>
-                    </div>
 
-                    <div className="flex justify-center font-light text-lg m-auto my-2">
-                        <p className="text-center">MN Tower Mongolia, J.Sambuu St, Улаанбаатар</p>
-                    </div>
-                </div>
-                <div className="mx-auto">
-                    <div className="flex justify-center">
-                        <div className="bg-primary-soft mx-5 rounded-full p-5 h-16 w-16 flex items-center justify-center">
-                            <HiOutlineUserGroup className='text-2xl' />
-                        </div>
-                    </div>
-                    <div className="flex justify-center font-bold text-xl m-auto my-2">
-                        <p className="text-center">{translate('locations')}</p>
-                    </div>
 
-                    <div className="flex justify-center font-light text-lg m-auto my-2">
-                        <p className="text-center">MN Tower Mongolia, J.Sambuu St, Улаанбаатар</p>
-                    </div>
-                </div>
 
             </div>
 
@@ -68,9 +78,9 @@ function Kontas() {
                 <div className="lg:w-3/5 md:w-4/5 sm:w-11/12 mx-auto  overflow-hidden">
                     <div className="flex flex-col md:flex-row">
                         <div className="md:w-1/2 md:p-5 lg:p-10 sm:p-10">
-                            <img className="object-cover lg:rounded-lg md:rounded-lg sm:rounded-none" src="https://source.unsplash.com/ihTs16sDPL8" alt="Modern building architecture" />
+                            <img className="object-cover shadow-xl lg:rounded-lg md:rounded-lg sm:rounded-none" src="/2.jpg" alt="Modern building architecture" />
                         </div>
-                        <div className="md:w-1/2 md:p-5 lg:p-10 sm:p-10">
+                        <div className="md:w-1/2 md:p-5 lg:p-10 sm:p-10 p-10">
                             <p className="block font-bold text-3xl leading-tight text-black">{translate('aboutUs')}</p>
                             <p className="mt-2 text-slate-500 lg:text-lg md:text-sm">{translate('info')}</p>
                         </div>
